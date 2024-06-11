3753a8701df534ca70eb5a4f0cb3672ffe54cbd9
langat68 <langatfidel32gmail.com>
2024-06-11 19:40:18 +0300

    initial add

---
+++ src/driver/driver.router.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+import { listDriver, getDriver, createDriver, updateDriver, deleteDriver } from "./driver.controller"
+import { zValidator } from "@hono/zod-validator";
+import { driverSchema } from "../validators";
+import { adminRoleAuth,userRoleAuth} from "../middleware/bearAuth";
+export const driverRouter = new Hono();
+
+//get all driver     /driver
+driverRouter.get("/driver",adminRoleAuth, listDriver);
+//get a single driver    /driver/1
+driverRouter.get("/driver/:id",userRoleAuth, getDriver)
+// create a driver
+driverRouter.post("/driver",userRoleAuth, createDriver)
+//update a driver
+driverRouter.put("/driver/:id",adminRoleAuth, updateDriver)
+
+driverRouter.delete("/driver/:id",adminRoleAuth, deleteDriver)
+
+//https:domai.com/driver?limit=10
